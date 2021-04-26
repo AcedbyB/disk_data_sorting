d33bfecee04cd2e2f89b7dea33271c5f9022c141
AcedbyB <baojustaced@gmail.com>
2021-04-27 00:31:48 +0700

    Starting on data generation part

--- data_generator.py
+++ data_generator.py
@@ -5,6 +5,9 @@ merge sort program.
 The schema definition should be separate from the data generation
 code. See example schema file `schema_example.json`.
 '''
+import random
+import string
+
 
 def generate_data(schema, out_file, nrecords):
   '''
@@ -23,18 +26,37 @@ def generate_data(schema, out_file, nrecords):
   The output file must be in csv format, with a new line
   character at the end of every record.
   '''
-  print "Generating %d records" % nrecords
+  f = open(output, "a")
+
+  for i in range(0, nrecords):
+    for field in schema:
+      if field["type"] == "string":
+        l = field["length"]
+        letters = string.ascii_lowercase
+        str_res = ''.join(random.choice(letters) for k in range(l)) 
+        f.write(str_res)
+      else:
+        mn = field["distribution"]["min"]
+        mx = field["distribution"]["max"]
+        value = random.randint(mn, mx)
+        f.write(str(value))
+      
+      f.write(",")
+    f.write("\n")
+
+  f.close()
+  print("Generating %d records" % nrecords)
 
 if __name__ == '__main__':
   import sys, json
   if not len(sys.argv) == 4:
-    print "data_generator.py <schema json file> <output csv file> <# of records>"
+    print("data_generator.py <schema json file> <output csv file> <# of records>")
     sys.exit(2)
 
   schema = json.load(open(sys.argv[1]))
   output = sys.argv[2]
   nrecords = int(sys.argv[3])
-  print schema
+  print(schema)
   
   generate_data(schema, output, nrecords)
 
